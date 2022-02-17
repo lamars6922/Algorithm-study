4abe9014a7d32d3c2da9c042ca4b760f4b224898
나화성 <@DESKTOP-CB0M8OI>
2022-02-17 23:56:48 +0900

    수정

--- Algorithm-study/src/test/LinkedListNode.java
+++ Algorithm-study/src/test/LinkedListNode.java
@@ -1,5 +1,7 @@
 package test;
 
+import test.LinkedList.Node;
+
 class LinkedList {
 	Node header;
 	
@@ -55,19 +57,105 @@ class LinkedList {
 			}
 			n = n.next;
 		}
-	} 
+	}
+
+	public Node getFirst() {
+		return header.next;
+	}
+	
+}
+
+class Reference {
+	public int count;
 }
 
 public class LinkedListNode {
 	public static void main(String[] args) {
 		LinkedList ll = new LinkedList();
-		System.out.println(ll.header.data);
-//		ll.append(1);
-//		ll.append(2);
-//		ll.append(3);
-//		ll.append(4);
-//		ll.retrieve();
+//		System.out.println(ll.header.data);
+		ll.append(1);
+		ll.append(2);
+		ll.append(3);
+		ll.append(4);
+		ll.retrieve();
 //		ll.delete(1);
 //		ll.retrieve();
+		
+//		int k = 2;
+//		Node kth = KthToLast(ll.getFirst(), k);
+//		System.out.println("Last k(" + k + ")th data is " + kth.data);
+///////////////////////////////////////////////////////////////////////////////////////////		
+//		int k = 2;
+//		KthToLast(head, k);
+///////////////////////////////////////////////////////////////////////////////////////////		
+		int k = 4;
+		Reference r = new Reference();
+		Node found = KthToLast(ll.getFirst(), k, r);
+		System.out.println(found.data);
+///////////////////////////////////////////////////////////////////////////////////////////		
+//		int k = 2;
+//		Node found = KthToLast(ll.getFirst(), k);
+//		System.out.println(found.data);
 	}
+	
+	// 뒤에서부터 k번째 노드 찾기
+	/*	
+		private static Node KthToLast(Node first, int k) { //재귀호출 사용하지 않고
+			Node n = first;
+			int total = 1;
+			while(n.next != null) {
+				total++;
+				n = n.next;
+			}
+			n = first;
+			for(int i = 1; i < total - k + 1; i++) {
+				n = n.next;
+			}
+			return n;
+		}
+		*/
+		/*
+		private static int KthToLast(Node n, int k) { //재귀호출 사용(값 출력)
+			if (n == null) {
+				return 0;
+			}
+			
+			int count = KthToLast(n.next, k) + 1;
+			if (count == k) {
+				System.out.println(k + "th to last node is " + n.data);
+			}
+			return count;
+		}
+		*/
+		
+		private static Node KthToLast(Node n, int k, Reference r) { //재귀호출 사용(노드 출력)
+			if (n == null) {
+				return null;
+			}
+			
+			Node found = KthToLast(n.next, k, r);
+			r.count++;
+			if (r.count == k) {
+				return n;
+			}
+			return found;
+		}
+		
+		/*
+		private static Node KthToLast(Node first, int k) { // 포인터사용
+			Node p1 = first;
+			Node p2 = first;
+			
+			for(int i = 0; i < k; i++) {
+				if(p1 == null) return null;
+				p1 = p1.next;
+			}
+			
+			while (p1 != null) {
+				p1 = p1.next;
+				p2 = p2.next;
+			}
+			return p2;
+		}
+		*/
 }
--- Algorithm-study/src/test/SinglyLinkedList.java
+++ Algorithm-study/src/test/SinglyLinkedList.java
@@ -38,10 +38,6 @@ class Node {
 	}
 }
 
-class Reference {
-	public int count;
-}
-
 public class SinglyLinkedList {
 	public static void main(String[] args) {
 		Node head = new Node(1);
@@ -49,79 +45,6 @@ public class SinglyLinkedList {
 		head.append(3);
 		head.append(4);
 		head.retrieve();
-		
-//		int k = 2;
-//		Node kth = KthToLast(head, k);
-//		System.out.println("Last k(" + k + ")th data is " + kth.data);
-///////////////////////////////////////////////////////////////////////////////////////////		
-//		int k = 2;
-//		KthToLast(head, k);
-///////////////////////////////////////////////////////////////////////////////////////////		
-//		int k = 2;
-//		Reference r = new Reference();
-//		Node found = KthToLast(head, k, r);
-//		System.out.println(found.data);
-///////////////////////////////////////////////////////////////////////////////////////////		
-		int k = 2;
-		Node found = KthToLast(head, k);
-		System.out.println(found.data);
 	}
-// 뒤에서부터 k번째 노드 찾기
-/*	
-	private static Node KthToLast(Node first, int k) { //재귀호출 사용하지 않고
-		Node n = first;
-		int total = 1;
-		while(n.next != null) {
-			total++;
-			n = n.next;
-		}
-		n = first;
-		for(int i = 1; i < total - k + 1; i++) {
-			n = n.next;
-		}
-		return n;
-	}
-	*/
-	/*
-	private static int KthToLast(Node n, int k) { //재귀호출 사용(값 출력)
-		if (n == null) {
-			return 0;
-		}
-		
-		int count = KthToLast(n.next, k) + 1;
-		if (count == k) {
-			System.out.println(k + "th to last node is " + n.data);
-		}
-		return count;
-	}
-	*/
-	/*
-	private static Node KthToLast(Node n, int k, Reference r) { //재귀호출 사용(값 출력)
-		if (n == null) {
-			return null;
-		}
-		
-		Node found = KthToLast(n.next, k, r);
-		r.count++;
-		if (r.count == k) {
-			return n;
-		}
-		return found;
-	}
-	*/
-	private static Node KthToLast(Node first, int k) { // 포인터사용
-		Node p1 = first;
-		Node p2 = first;
-		
-		for(int i = 0; i < k; i++) {
-			if(p1 == null) return null;
-			p1 = p1.next;
-		}
-		
-		while (p1 != null) {
-			p1 = p1.next;
-			p2 = p2.next;
-		}
-		return p2;
-	}
-}
+
+} 
