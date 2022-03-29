8e5b216251e0b8cfff0859845396f5faad0cffde
나화성 <@DESKTOP-CB0M8OI>
2022-03-29 23:14:55 +0900

    Tree에서 두노드의 첫번째 공통부모 2번째

--- Algorithm-study/src/test/SearchCommonNode.java
+++ Algorithm-study/src/test/SearchCommonNode.java
@@ -33,6 +33,14 @@ class Tree1 {
 	Node getNode(int data) {
 		return rootMap.get(data);
 	}
+	class Result {
+		Node node;
+		boolean isAncestor;
+		Result (Node n, boolean isAnc) {
+			node = n;
+			isAncestor = isAnc;
+		}
+	}
 	Node commonAncestor1(int d1, int d2) {
 		Node p = getNode(d1);
 		Node q = getNode(d2);
@@ -112,6 +120,58 @@ class Tree1 {
 		Node childSide = pIsOnLeft? root.left : root.right;
 		return ancestorHelper(childSide, p, q);
 	}
+	Node commonAncestor4(int d1, int d2) {
+		Node p = getNode(d1);
+		Node q = getNode(d2);
+		return commonAncestor4(root, p, q);
+	}
+	Node commonAncestor4(Node root, Node p, Node q) { // 단점 : 한 노드는 트리에 없을 경우 한개만 찾은 값을 공통분모라고 생각한다.
+		if(root == null) return null;
+		if(root == p && root == q) return root;
+		Node x = commonAncestor4(root.left, p, q);
+		if(x != null && x != p && x !=q) {
+			return x;
+		}
+		Node y = commonAncestor4(root.right, p, q);
+		if(y != null && y != p && y != q) { 
+			return y;
+		}
+		
+		if(x != null && y != null) { 
+			return root;
+		} else if(root == p || root == q) { // 둘 중 하나가 자신일 경우
+			return root;
+		} else {
+			return x == null ? y : x; // 둘 중 하나를 찾거나 둘 다 못 찾을 때
+		}
+	}
+	
+	Node commonAncestor5(int d1, int d2) {
+		Node p = getNode(d1);
+		Node q = getNode(d2);
+		Result r = commonAncestor5(root, p, q);
+		if(r.isAncestor) {
+			return r.node;
+		}
+		return null;
+	}
+	Result commonAncestor5(Node root, Node p, Node q) {
+		if(root == null) return new Result(null, false);
+		if(root == p && root == q) return new Result(root, true);
+		Result rx = commonAncestor5(root.left, p, q);
+		if(rx.isAncestor) return rx;
+		Result ry = commonAncestor5(root.right, p, q);
+		if(ry.isAncestor) return ry;
+		
+		if(rx.node != null && ry.node != null) { 
+			return new Result(root, true);
+		} else if(root == p || root == q) { // 둘 중 하나가 자신일 경우
+			boolean isAncestor = rx.node != null || ry.node != null;
+			return new Result(root, isAncestor);
+		} else {
+			return new Result(rx.node != null ? rx.node : ry.node, false); // 둘 중 하나를 찾거나 둘 다 못 찾을 때
+		}
+	}
 }
 public class SearchCommonNode {
 
@@ -121,9 +181,13 @@ public class SearchCommonNode {
 		Tree1.Node fa1 = t.commonAncestor1(3, 5);
 		Tree1.Node fa2 = t.commonAncestor2(6, 7);
 		Tree1.Node fa3 = t.commonAncestor3(2, 8);
+		Tree1.Node fa4 = t.commonAncestor4(6, 8);
+		Tree1.Node fa5 = t.commonAncestor5(0, 3);
 		System.out.println("The first common ancestor is " + fa1.data);
 		System.out.println("The first common ancestor is " + fa2.data);
 		System.out.println("The first common ancestor is " + fa3.data);
+		System.out.println("The first common ancestor is " + fa4.data);
+		System.out.println("The first common ancestor is " + fa5.data);
 	}
 
 }
